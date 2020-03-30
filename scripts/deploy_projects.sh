f769984d348060412164410ec7c9a8f8e86ffeed
stdmatt <stdmatt@pixelwizards.io>
2020-03-30 14:38:27 +0300

    Continue the deployment scritp

--- scripts/deploy_projects.sh
+++ scripts/deploy_projects.sh
@@ -8,21 +8,23 @@ SCRIPT_DIR="$(pw_get_script_dir)";
 PROJECT_ROOT="$(pw_abspath "${SCRIPT_DIR}/..")";
 PROJ_REPOS_DIR="${PROJECT_ROOT}/_project_repos";
 OUTPUT_DIR="${PROJECT_ROOT}/_output/deploy";
-RULES_DIR="${SCRIPT_DIR}/deploy_rules";
+BUILD_RULES_DIR="${SCRIPT_DIR}/build_rules";
+DEPLOY_RULES_DIR="${SCRIPT_DIR}/deploy_rules";
 
+    # demos/metaballs    \
+    # demos/simple_clock \
+    # demos/simple_tree  \
+    # demos/starfield    \
+    #        \
+    # games/color_grid   \
+    # games/nuclear_rain \
+    # games/simple_snake \
+    #        \
+    # games/old_old_old_games/kaboom \
+    # games/old_old_old_games/ramit  \
 PROJECTS=" \
-    demos/metaballs    \
-    demos/simple_clock \
-    demos/simple_tree  \
-    demos/starfield    \
-           \
-    games/color_grid   \
-    games/nuclear_rain \
-    games/simple_snake \
-    games/kaboom       \
-    games/ramit        \
+    games/old_old_old_games/taz    \
 ";
-
 
 
 gitlab_url()
@@ -32,7 +34,7 @@ gitlab_url()
 
 
 
-update_project_to_latest_tag()
+deploy_project()
 {
     local PROJECT="$1";
     local PROJECT_PATH="${PROJ_REPOS_DIR}/$PROJECT";
@@ -40,9 +42,11 @@ update_project_to_latest_tag()
 
     pw_pushd "$PROJECT_PATH";
 
+    ##
     ## Fetch...
     git fetch --tags;
 
+    ##
     ## Grab the latest tag.
     local TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2> /dev/null);
     test -z "$TAG" \
@@ -50,31 +54,46 @@ update_project_to_latest_tag()
 
     pw_func_log "$(pw_FC "$PROJECT") Version: $(pw_FM "($TAG)")";
 
+    ##
     ## Go to the latest version.
     git checkout $TAG
     git submodule update --init --recursive;
 
-    ## Build.
-    pw_func_log "$(pw_FC "$PROJECT")$(pw_FM "($TAG)") - $(pw_FB Building...)";
-    ./scripts/build.sh --dist;
 
-    ## Move to the ouput...
-    mkdir -p    "$PROJECT_OUTPUT_PATH";
-    mv ./dist/* "$PROJECT_OUTPUT_PATH";
-
-    ## Deploy...
-    pw_func_log "$(pw_FC "$PROJECT")$(pw_FM "($TAG)") - $(pw_FB Deploying...)";
     local PROJECT_NAME="$(basename "$PROJECT")";
 
+    ##
+    ## Build.
+    pw_func_log "$(pw_FC "$PROJECT")$(pw_FM "($TAG)") - $(pw_FB Building...)";
+
+    ## Project has some special build rules...
+    if [ -f "${BUILD_RULES_DIR}/${PROJECT_NAME}.sh" ]; then
+        "${BUILD_RULES_DIR}/${PROJECT_NAME}.sh"
+    ## Project has not special build steps...
+    else
+        ./scripts/build.sh --dist;
+    fi;
+
+
+    ##
+    ## Move build artifacts to the output...
+    mkdir -p    "$PROJECT_OUTPUT_PATH";
+    find ./dist -maxdepth 1 -type f -exec mv {} "$PROJECT_OUTPUT_PATH" \;
+
+    ##
+    ## Deploy...
+    pw_func_log "$(pw_FC "$PROJECT")$(pw_FM "($TAG)") - $(pw_FB Deploying...)";
+
     ## Project has some special deployment rules...
-    if [ -f "${RULES_DIR}/${PROJECT_NAME}.sh" ]; then
-        "${RULES_DIR}/${PROJECT_NAME}.sh"
-    ## Project has not special deploymetn steps... It's just unzip and fun!!!
+    if [ -f "${DEPLOY_RULES_DIR}/${PROJECT_NAME}.sh" ]; then
+        "${DEPLOY_RULES_DIR}/${PROJECT_NAME}.sh"
+    ## Project has not special deployment steps... It's just unzip and fun!!!
     else
         pw_pushd "$PROJECT_OUTPUT_PATH"
             unzip -o *.zip
         pw_popd;
     fi;
+
     pw_popd;
 }
 
@@ -93,6 +112,6 @@ mkdir -p "${OUTPUT_DIR}";
 
 for PROJECT in $PROJECTS; do
     echo "Deploying project: ($(pw_FG $PROJECT))";
-    init_repository              "$PROJECT";
-    update_project_to_latest_tag "$PROJECT";
+    init_repository "$PROJECT";
+    deploy_project  "$PROJECT";
 done;
