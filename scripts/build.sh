9390253a1ecdf3044fbced66e9ffdb97fb780e08
stdmatt <stdmatt@pixelwizards.io>
2020-04-08 16:15:12 +0300

    Create the overall build script

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source /usr/local/src/stdmatt/shellscript_utils/main.sh
+
+SCRIPT_DIR="$(pw_get_script_dir)";
+
+## @todo(stdmatt): Improve the script to accept what we want to build...
+${SCRIPT_DIR}/generate_html_files.sh
+${SCRIPT_DIR}/deploy_projects.sh
+${SCRIPT_DIR}/copy_static_files.sh
