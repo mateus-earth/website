e8dbbe974315e60681ebafbbc3e51935312e7772
stdmatt <stdmatt@pixelwizards.io>
2022-04-13 12:16:08 -0300

    ???

--- source/main.js
+++ source/main.js
@@ -1,3 +1,9 @@
+const DEMOS_LIST = [
+    // "flowfield",
+    // "starfield",
+    "roses",
+]
+
 //------------------------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", ()=> {
     _on_ready();
@@ -6,6 +12,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
 //------------------------------------------------------------------------------
 function _on_ready()
 {
+    //
+    // Create the default canvas...
+    //
+
     const canvas = document.createElement("canvas");
 
     canvas.width            = window.innerWidth;
@@ -18,17 +28,24 @@ function _on_ready()
     document.body.appendChild(canvas);
     var ctx = canvas.getContext("2d");
     var grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-    grd.addColorStop(0, "yellow");
-    grd.addColorStop(0.5, "blue");
-    grd.addColorStop(1, "white");
+    grd.addColorStop(0,   "yellow");
+    grd.addColorStop(0.5, "blue"  );
+    grd.addColorStop(1,   "white" );
 
-    // Fill with gradient
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const demo_path_prefix   = "/modules/demos/starfield/";
-    const demo_main_filename = "source/starfield.js";
+    //
+    // Load the demo...
+    //
+    const demo_index         = Math.trunc(Math.random() * DEMOS_LIST.length);
+    const demo_name          = DEMOS_LIST[demo_index];
+    const demo_path_prefix   = "/modules/demos/" + demo_name;
+    const demo_main_filename = "/source/main.js";
 
+    // @notice: [Load order] at 22-03-06, 13:45
+    //   Load the demo's main file, with that we have
+    //   the info of all files that that demo requires...
     demolib_load_script(demo_main_filename, demo_path_prefix).then(()=>{
         demolib_load_all_scripts(__SOURCES, demo_path_prefix).then(()=> {
             demo_start(canvas);
